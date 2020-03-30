438799cbf03011bcffa44faada805d146827f93a
NikolayR21 <1081601@ucn.dk>
2020-03-30 14:41:03 +0200

    Information js ready

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,10 @@ const homePostId = 23;
 const informationPostId = 24;
 const performanceId = 22;
 const tagArtistId = 21;
+const tagStagesId = 15;
 
 
-getHomePostFromWP();
+
 
 function getHomePostFromWP() {
     const xhttp = new XMLHttpRequest();
@@ -48,3 +49,83 @@ function renderHomeData(homeData) {
     }
 }
 
+getInformationPostFromWP();
+
+function getInformationPostFromWP() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            const informationData = JSON.parse(this.responseText).reverse();
+            console.log(informationData);
+            renderInformationData(informationData);
+        }
+    }
+    xhttp.open('GET', `${apiUrl}posts?categories=${informationPostId}`, true);
+    xhttp.setRequestHeader('Authorization', `Bearer ${apiKey}`);
+    xhttp.send();
+}
+
+function getStagesFromWP() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            const stagesData = JSON.parse(this.responseText).reverse();
+            console.log(stagesData);
+            renderStagesData(stagesData);
+        }
+    }
+    xhttp.open('GET', `${apiUrl}posts?tags=${tagStagesId}`, true);
+    xhttp.setRequestHeader('Authorization', `Bearer ${apiKey}`);
+    xhttp.send();
+}
+
+function renderInformationData(informationData) {
+    for (i = 0; i < informationData.length; i++) {
+        if (informationData[i].acf.heading === 'Stages:') {
+            document.querySelector('section').innerHTML += `
+             <article id="infoToggle${i}" onclick="toggleInfo(${i})">
+             <div class="flex-info">
+                 <h5>${informationData[i].acf.heading}</h5>
+                 <i class="fas fa-angle-down" for="club" class="info-arrow"></i>
+             </div>
+             <div class="stages-info">
+             <br>
+             <p>${informationData[i].acf.information}</p>
+             <br>
+            
+             </div>
+         </article>
+         `;
+        } else {
+            document.querySelector('section').innerHTML += `
+            <article id="infoToggle${i}" onclick="toggleInfo(${i})">
+            <div class="flex-info">
+                <h5>${informationData[i].acf.heading}</h5>
+                <i class="fas fa-angle-down" for="club" class="info-arrow"></i>
+            </div>
+            <p>${informationData[i].acf.information}
+            </p>
+            <hr>
+        </article>
+        `;
+        }
+
+    }
+    getStagesFromWP();
+}
+
+function renderStagesData(stagesData) {
+    let stagesInfo = document.querySelector('.stages-info');
+    for (i = 0; i < stagesData.length; i++) {
+        let currentStage = stagesData[i].acf;
+        stagesInfo.innerHTML += `
+        <h6>${currentStage.name}</h6>
+        <p>${currentStage.description}</p>
+        <br>
+        `
+    }
+}
+
+function toggleInfo(i) {
+    document.getElementById('infoToggle' + i).classList.toggle('open');
+}
